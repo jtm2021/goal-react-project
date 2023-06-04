ceb5c7d5ec90c989f83456c1319f66d2fca2405c
jtm2021 <91266239+jtm2021@users.noreply.github.com>
2023-06-04 21:52:47 +0100

    Change button css styling

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -6,7 +6,7 @@ const Button = styled.button`
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
   color: white;
-  background: #8b005d;
+  background: #005501;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
@@ -20,7 +20,7 @@ const Button = styled.button`
 
   &:hover,
   &:active {
-    background: #ac0e77;
+    background: #1bd114;
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
